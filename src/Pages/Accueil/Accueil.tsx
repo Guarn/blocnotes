1e342dc87426b32a5e47254a51e3c7dca64e61ab
Guarn <florianjung885@gmail.com>
2021-11-14 10:27:41 +0100

    Correction Historique (title)

--- src/Pages/Accueil/Accueil.tsx
+++ src/Pages/Accueil/Accueil.tsx
@@ -1,34 +1,40 @@
+import { useEffect } from 'react';
 import * as S from './Accueil.styled';
 import BlocLien from './BlocLien';
 
-const Accueil = () => (
-  <S.AccueilGlobal>
-    <S.ActifCtn>
-      <BlocLien
-        couleur="#B57676"
-        icone="BlocNotes"
-        titre="BLOC NOTES"
-        lien="/bloc-notes"
-      />
-      <BlocLien
-        couleur="#7693B5"
-        icone="Calculette"
-        titre="CALCULETTE"
-        lien="/calculette"
-      />
-      <BlocLien
-        couleur="#77B576"
-        icone="Messages"
-        titre="MESSAGES"
-        lien="/messages"
-      />
-    </S.ActifCtn>
-    <S.ActifCtn>
-      <BlocLien />
-      <BlocLien />
-      <BlocLien />
-    </S.ActifCtn>
-  </S.AccueilGlobal>
-);
+const Accueil = () => {
+  useEffect(() => {
+    document.title = 'Accueil';
+  }, []);
+  return (
+    <S.AccueilGlobal>
+      <S.ActifCtn>
+        <BlocLien
+          couleur="#B57676"
+          icone="BlocNotes"
+          titre="BLOC NOTES"
+          lien="/bloc-notes"
+        />
+        <BlocLien
+          couleur="#7693B5"
+          icone="Calculette"
+          titre="CALCULETTE"
+          lien="/calculette"
+        />
+        <BlocLien
+          couleur="#77B576"
+          icone="Messages"
+          titre="MESSAGES"
+          lien="/messages"
+        />
+      </S.ActifCtn>
+      <S.ActifCtn>
+        <BlocLien />
+        <BlocLien />
+        <BlocLien />
+      </S.ActifCtn>
+    </S.AccueilGlobal>
+  );
+};
 
 export default Accueil;
--- src/Pages/Navigation/Navigation.tsx
+++ src/Pages/Navigation/Navigation.tsx
@@ -1,22 +1,16 @@
-import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Accueil from '../Accueil/Accueil';
 import BlocNotes from '../BlocNotes/BlocNotes';
 import Calculette from '../Calculette/Calculette';
 import Messages from '../Messages/Messages';
 
-const Navigation = () => {
-  useEffect(() => {
-    document.title = 'Accueil';
-  }, []);
-  return (
-    <Routes>
-      <Route path="/" element={<Accueil />} />
-      <Route path="/bloc-notes" element={<BlocNotes />} />
-      <Route path="/calculette" element={<Calculette />} />
-      <Route path="/messages" element={<Messages />} />
-    </Routes>
-  );
-};
+const Navigation = () => (
+  <Routes>
+    <Route path="/" element={<Accueil />} />
+    <Route path="/bloc-notes" element={<BlocNotes />} />
+    <Route path="/calculette" element={<Calculette />} />
+    <Route path="/messages" element={<Messages />} />
+  </Routes>
+);
 
 export default Navigation;
